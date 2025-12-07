02f3232fe4c3977f397422eb44668f2179e1fbc0
Resham <resha@MacBook-Air-2.local>
2025-12-06 21:34:00 -0800

    Added Sum_ascii function

---
+++ lab-37-starter-2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int sum_ascii(string str);
+
+int main() 
+{
+    int sum = sum_ascii("Hello");
+    cout << sum << endl;
+
+    return 0;
+}
+
+int sum_ascii(string str)
+{
+    int sum;
+    for (char c : str) 
+    {
+        sum += (int) c;
+    }
+    
+    return sum;
+}
+
+/* 
+These targets are present in the dataset and can be used for testing:
+536B9DFC93AF
+1DA9D64D02A0
+666D109AA22E
+E1D2665B21EA
+*/
